2e65e2886b8df57f49e90d0f40979fe22fcbd665
Marko Pavlović <markopavlovic316@gmail.com>
2023-01-06 14:57:08 +0100

    Completing the design with custom template and Bootstrap, leaving Foundation to the bare minimum.

---
+++ www/bluefreedom.hr/custom/components/footer.php
@@ -0,0 +1,1 @@
+<div class="footer">Copyright &copy; <?php echo date('Y');?> &mdash; BlueFreedom</div>
---
+++ www/bluefreedom.hr/custom/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "components/header.php"?>
+
+<body class="my-login-page">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+						<a href="./afterlogin/"><img src="afterlogin/img/png.png" alt="logo"></a>
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title">Login</h4>
+							<form method="POST" class="my-login-validation" novalidate="">
+								<div class="form-group">
+									<label for="email">E-Mail Address</label>
+									<input id="email" type="email" class="form-control" name="email" value="" required autofocus>
+									<div class="invalid-feedback">
+										Email is invalid
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="password">Password
+										<a href="forgot.php" class="float-right">
+											Forgot Password?
+										</a>
+									</label>
+									<input id="password" type="password" class="form-control" name="password" required data-eye>
+								    <div class="invalid-feedback">
+								    	Password is required
+							    	</div>
+								</div>
+
+								<div class="form-group">
+									<div class="custom-checkbox custom-control">
+										<input type="checkbox" name="remember" id="remember" class="custom-control-input">
+										<label for="remember" class="custom-control-label">Remember Me</label>
+									</div>
+								</div>
+
+								<div class="form-group m-0">
+									<button type="submit" class="btn btn-primary btn-block">
+										Login
+									</button>
+								</div>
+								<div class="mt-4 text-center">
+									Don't have an account? <a href="register.php">Create One</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<?php include "components/footer.php"?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php include "components/scripts.php"?>
+</body>
+</html>
